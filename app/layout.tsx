2f3e9377ea5fa35fcdb2145f69a163eb45069db0
Denise Diori <netvybeusa@gmail.com>
2026-04-27 19:26:47 -0500

    Fix Providers filename casing for Linux build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Providers from "./providers";
+import Providers from "./Providers";
 import { ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
